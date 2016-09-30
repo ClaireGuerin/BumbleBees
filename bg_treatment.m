87709a4774330d93adca2c101ebc9b1092895c45
Zach Werkhoven <werkhoven@fas.harvard.edu>
2016-09-30 14:38:46 -0400

    Added preliminary script of trials for separation between background and bumble bees

---
+++ bg_treatment.m
@@ -0,0 +1,104 @@
+[filename, pathname] = uigetfile('*.avi'); % user input: movie file name and path
+
+mov = VideoReader([pathname filename]); % import file
+
+%%
+nframes = mov.NumberOfFrames; % number of frames in the video
+
+for i = 1:nframes % for each frame
+%%
+    im = read(mov,i); % read frame
+    imshow(im); % show frame
+
+    % insert tracking code
+    figure(1);
+    codes = locateCodes(im);
+    figure(2);
+    tic
+    codes = locateCodes(im, 'threshMode', 1, 'bradleyFilterSize', [10 10], 'bradleyThreshold', 3);
+    toc % processing delay
+    drawnow
+end
+
+im = rgb2gray(im); % convert image from color to grey scale
+
+%frame1 = read(mov,1);
+%col = [];
+%allStd = []
+
+%%
+%for i = 1:size(frame1,1);
+%    for j = 1:size(frame1,2);
+%        for k = 1:nframes;
+%            col(k) = im(i,j);
+%        end
+%        allStd(i,j) = std(col);
+%    end
+%end
+%%
+
+nBackFrames = 20;
+backFrameInds = round(linspace(1,nframes,nBackFrames)); % regularly spaced-out frame indices
+
+%Create empty matrix for background calculation
+backCalc = nan(mov.Height, mov.Width,nBackFrames);
+for i = 1:nBackFrames
+    im = read(mov,backFrameInds(i));
+    imshow(im);
+    im = rgb2gray(im);
+    backCalc(:,:,i) = im;
+end
+
+backImage = median(backCalc,3); % try also mode or mean --> which one renders a better background mask?
+
+%% Example background subtraction for single image
+
+imCur  = read(mov,100);
+imCur = rgb2gray(imCur);
+
+imDiff = abs(imCur - uint8(backImage));
+
+intThresh = 5;
+
+%Threshold into binary image
+imBW = imDiff > intThresh;
+
+%%Test how bg is altered by erosion and dilation
+test = uint8(backImage);
+se = offsetstrel('ball',5,5);
+erodedI = imerode(test,se);
+dilatedI = imdilate(test,se);
+erodilatedI = imdilate(dilatedI,se);
+figure(1);
+subplot(2,2,1);
+imshow(test); title('original background')
+subplot(2,2,2);
+imshow(erodedI); title('eroded background')
+subplot(2,2,3);
+imshow(dilatedI); title('dilated background')
+subplot(2,2,4);
+imshow(erodilatedI); title('eroded & dilated background');
+%%
+
+%%Compare efficiency in tag recognition under different image treatments
+imDiff2 = abs(imCur - uint8(erodilatedI));
+figure(2);
+subplot(3,2,1);
+imshow(imCur); title('Original Image');
+subplot(3,2,2);
+tic
+codes = locateCodes(imCur); title('Identified tags')
+toc
+subplot(3,2,3)
+imshow(imDiff); title('Filtered Img')
+subplot(3,2,4);
+tic
+codes = locateCodes(imDiff); title('Identified tags')
+toc
+subplot(3,2,5)
+imshow(imDiff2); title('Filtered Img, Eroded+Dilated Bg')
+subplot(3,2,6);
+tic
+codes = locateCodes(imDiff2); title('Identified tags')
+toc
+%%
