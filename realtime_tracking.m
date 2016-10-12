0cf447ea9c20a679f4c5d5879625127886b0e930
Zach Werkhoven <werkhoven@fas.harvard.edu>
2016-10-12 18:07:16 -0400

    Okay this should work as real-time tracking, but need to check on actual bees to see how robust and fast it is...

--- realtime_tracking.m
+++ realtime_tracking.m
@@ -23,7 +23,7 @@ start(vid)
 %% Background Calculation
 
 BAD = 20;   % background acquisition duration (in seconds)
-depth = 30;    % number of images used for creating background
+depth = 30;    % number of images to use for creating background
 backIm = calculateBackground(vid,BAD,depth);
 imshow(backIm);
 
@@ -38,12 +38,19 @@ tic
 while toc < expDuration
     im = peekdata(vid,1);
     [beePos, beeRad] = spotBees(im, backIm, intThresh);
+    imshow(im)
+    
+    hold on
    
     for i = 1:size(beePos,1)
         beeX = beePos(i,1);
         beeY = beePos(i,2);
         subIm = imcrop(im, [beeX-beeRad*2 beeY-beeRad*2 beeRad*4 beeRad*4]); %[xmin ymin width height]
         codes = locateCodes(subImage, vis, 0); % identify bee codes without showing cropped image
+        plot(beeX, beeY, 'r*');
+        txt1 = codes.number;
+        text(beeX,beeY,txt1, Color, [250 0 0])
+    end 
     
-    end    
+    hold off
 end
